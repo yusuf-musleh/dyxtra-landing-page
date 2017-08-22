d77b42506caccd21cad90eb3205396d80902d512
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-08-22 01:15:48 -0400

    Enforce all requests though ssl in production (#10)
    * Add decorator to redirect all requests to go through https in production

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 
 import os
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from flask_mail import Mail
 
 from werkzeug.contrib.fixers import ProxyFix
 
+from functools import wraps
 from helpers import send_email
 
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_SESSIONS_SECRET_KEY']
+
+if os.environ.get('PRODUCTION'):
+	app.config['DEBUG'] = False
+	app.config['TESTING'] = False
+else:
+	app.config['DEBUG'] = True
+	app.config['TESTING'] = True
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -24,17 +32,36 @@ mail = Mail(app)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
+
+# decorator to redirect all requests to go through https
+def ssl_required(fn):
+    @wraps(fn)
+    def decorated_view(*args, **kwargs):
+        if not (app.debug or app.testing):
+            if request.is_secure:
+                return fn(*args, **kwargs)
+            else:
+                return redirect(request.url.replace("http://", "https://"))
+
+        return fn(*args, **kwargs)
+
+    return decorated_view
+
+
 @app.route('/', methods=['GET'])
+@ssl_required
 def index():
 	return render_template('index.html')
 
 
 @app.route('/pricing', methods=['GET'])
+@ssl_required
 def pricing():
 	return render_template('pricing.html')
 
 
 @app.route('/register', methods=['GET'])
+@ssl_required
 def register():
 	selected_plan = request.args.get('selected_plan')
 	session['selected_plan'] = selected_plan
